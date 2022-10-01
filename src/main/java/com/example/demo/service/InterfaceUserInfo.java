d2f319423914c37e11dd3c3790a36ee57c79f040
YangXiaoping <1393371859@qq.com>
2022-10-01 21:40:21 +0800

    添加token验证

---
+++ src/main/java/com/example/demo/service/InterfaceUserInfo.java
@@ -0,0 +1,7 @@
+package com.example.demo.service;
+
+import com.example.demo.model.UserInfo;
+
+public interface InterfaceUserInfo {
+    void insertOneUser(UserInfo userInfo);
+}
---
+++ src/main/java/com/example/demo/service/impl/UserInfoServiceImpl.java
@@ -0,0 +1,13 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.model.UserInfo;
+import com.example.demo.service.InterfaceUserInfo;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserInfoServiceImpl implements InterfaceUserInfo {
+    @Override
+    public void insertOneUser(UserInfo userInfo){
+
+    }
+}
