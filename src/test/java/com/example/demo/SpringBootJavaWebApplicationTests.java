04a5875dbc371f7770502bbab96342c1983d38eb
YangXiaoping <1393371859@qq.com>
2022-10-02 16:39:17 +0800

    提交邮箱，发送验证码和邮箱发送运行问题

--- src/test/java/com/example/demo/SpringBootJavaWebApplicationTests.java
+++ src/test/java/com/example/demo/SpringBootJavaWebApplicationTests.java
@@ -2,13 +2,13 @@ package com.example.demo;
 
 import com.example.demo.base.RedisBase;
 import com.example.demo.mapper.UserAccountMapper;
-import com.example.demo.model.UserAccount;
+import com.example.demo.util.EmailUtil;
 import com.example.demo.util.RedisUtil;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
-
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import javax.annotation.Resource;
 
 @SpringBootTest
@@ -16,11 +16,21 @@ class SpringBootJavaWebApplicationTests {
 	//这是redis封装的类
 	@Resource
 	private RedisUtil redisUtil;
+	@Resource
+	private JavaMailSender javaMailSender ;
+
 	@Test
 	void contextLoads() {
-		redisUtil.set("name","杨小平");
-		redisUtil.get("name");
-		System.out.println(redisUtil.get("name"));
+//		redisUtil.get("name");
+		SimpleMailMessage message = new SimpleMailMessage() ;	// 要发送的消息内容
+		message.setFrom("1393371859@qq.com");
+		message.setTo("19301105@bjtu.edu.cn");
+		message.setSubject("测试邮件");
+		message.setText("好好学习，天天向上");
+		this.javaMailSender.send(message);
+
+		EmailUtil email = new EmailUtil();
+		System.out.println(1111);
 	}
 
 }
