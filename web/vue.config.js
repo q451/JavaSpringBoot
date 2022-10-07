790d77811fb2d4c641ed37b504f28bc44a71bc47
YangXiaoping <1393371859@qq.com>
2022-10-07 20:46:45 +0800

    前端代码提交

---
+++ web/vue.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    baseUrl: './',
+    assetsDir: 'static',
+    productionSourceMap: false,
+    // devServer: {
+    //     proxy: {
+    //         '/api':{
+    //             target:'http://localhost:9001',
+    //             changeOrigin:true,
+    //             pathRewrite:{
+    //                 '/api':''
+    //             }
+    //         }
+    //     }
+    // }
+}
